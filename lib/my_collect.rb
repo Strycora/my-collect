f632c467cc88161264b605420d89ba70e9660230
Vikram Chatterjee <strycora@gmail.com>
2020-11-11 13:53:05 -0700

    commit tree a2a371de3f7a395e529ee01d4389cbd5cc2f31f7

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
 
+def my_collect(array) #creates a new method
+    i = 0 #initializes a counter
+    collection = [] #creates a new array to store collected objects in 
+    while i < array.length #creates a loop for iteration over array
+        collection << yield(array[i]) #adds the output of the methods in mycollect_spec to the new array
+        i+=1 #increments the counter for each iteration
+    end
+    collection #returns the new array 
 
+
+end
